524be116d1a7428ff778c0299a95ecd37859be7a
codemaster138 <pygamer138@gmail.com>
2021-04-24 18:59:12 +0200

    docs(Card): Update docs for card

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export { default as Button } from "./components/button";
 export { default as Input } from "./components/input";
 export { default as StepBar} from "./components/step-bar";
 export { default as IconButton } from "./components/icon-button";
-export { default as Card } from "./components/card";
+export { default as Card, CardHeading, CardForm } from "./components/card";
 export { default as OceanRoot } from "./components/root";
 export { default as defaultLightTheme } from "./themes/light";

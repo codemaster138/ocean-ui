2d1f56c6582274b7520d1c32ae675a816845b948
codemaster138 <pygamer138@gmail.com>
2021-04-24 17:32:12 +0200

    docs(storybook): Add ocean logo to theme

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import oceanTheme from './oceanTheme';
+
+addons.setConfig({
+  theme: oceanTheme,
+});
---
+++ .storybook/oceanTheme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'OCEAN Design',
+  brandImage: 'https://siasky.net/XAF_z8pS172NaAS_zqn_gJJg01dJauR95Qi98KVXJY9Sng',
+});
